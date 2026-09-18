abae7ea028b7c162a0daa3bd51746cf7bcd48677
Thomas Gouzecky <thomasjgouzecky@gmail.com>
2026-09-18 18:56:51 -0400

    added routes to the api

--- apps/db-api/app/main.py
+++ apps/db-api/app/main.py
@@ -2,7 +2,12 @@ from fastapi import FastAPI
 
 from app.sql import SessionDep, create_db_and_tables
 
+from app.routes import messages, groupchats
+
 app = FastAPI()
+
+app.include_router(messages.router)
+app.include_router(groupchats.router)
 
 
 @app.on_event("startup")

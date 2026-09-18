ac6012dac6a12849d2be2181fbc67e1ebdbce550
Thomas Gouzecky <thomasjgouzecky@gmail.com>
2026-09-18 17:46:53 -0400

    added messages model

---
+++ apps/db-api/app/models/Messages.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+
+from sqlmodel import Field, SQLModel, Column
+from sqlalchemy import DateTime, ForeignKey, Integer, Text, text
+
+
+class Messages(SQLModel, table=True):
+    __tablename__: str = "Messages"
+
+    id: int | None = Field(sa_column=Column(Integer, default=None, primary_key=True))
+    user_id: str = Field(sa_column=Column(Text, nullable=False))
+    group_chat_id: int = Field(
+        sa_column=Column(
+            Integer,
+            ForeignKey("GroupChats.id"),
+            nullable=False,
+        )
+    )
+    message: str = Field(sa_column=Column(Text, default=""))
+    created_at: datetime = Field(
+        sa_column=Column(
+            DateTime(timezone=True),
+            server_default=text("CURRENT_TIMESTAMP"),
+            nullable=False,
+        ),
+    )
+    modified_at: datetime = Field(
+        sa_column=Column(
+            DateTime(timezone=True),
+            server_default=text("CURRENT_TIMESTAMP"),
+            nullable=False,
+        )
+    )

d5e0cc5f79d4c77c42731f6c34ac9e50316f7d17
Thomas Gouzecky <thomasjgouzecky@gmail.com>
2026-09-24 17:13:33 -0400

    added swagger for documentation

--- apps/backend/Program.cs
+++ apps/backend/Program.cs
@@ -6,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
@@ -73,6 +75,8 @@ if (args.Contains("--migrate", StringComparer.OrdinalIgnoreCase))
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseExceptionHandler();

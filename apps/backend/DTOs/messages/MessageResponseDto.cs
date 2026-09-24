419add74a9960d6681ffc212769802eae5a1e629
Thomas Gouzecky <thomasjgouzecky@gmail.com>
2026-09-24 17:59:41 -0400

    updated authentication and messing dtos to map correctly to data

--- apps/backend/DTOs/messages/MessageResponseDto.cs
+++ apps/backend/DTOs/messages/MessageResponseDto.cs
@@ -1,9 +1,16 @@
+using System.Text.Json.Serialization;
+
 public class MessageResponseDto
 {
     public int Id { get; set; }
+    [JsonPropertyName("content")]
     public string Message { get; set; } = string.Empty;
+    [JsonPropertyName("user_id")]
     public string UserId { get; set; } = string.Empty;
+    [JsonPropertyName("group_chat_id")]
     public int GroupChatId { get; set; }
+    [JsonPropertyName("created_at")]
     public DateTime CreatedAt { get; set; }
+    [JsonPropertyName("modified_at")]
     public DateTime ModifiedAt { get; set; }
 }
--- apps/backend/Program.cs
+++ apps/backend/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = false)
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddSignInManager();
 builder.Services.ConfigureApplicationCookie(options =>
